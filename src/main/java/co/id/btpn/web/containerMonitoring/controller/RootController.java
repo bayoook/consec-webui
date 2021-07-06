353ad06f2af31215bdd006d1121af16f34618c60
Ferry Fadly <ferry.fadly@gmail.com>
2021-07-06 17:15:12 +0700

    add kibana url properties

--- src/main/java/co/id/btpn/web/containerMonitoring/controller/RootController.java
+++ src/main/java/co/id/btpn/web/containerMonitoring/controller/RootController.java
@@ -3,8 +3,11 @@ package co.id.btpn.web.containerMonitoring.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -18,13 +21,17 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("attributes")
 public class RootController {
 
-
+    @Value("${kibana.dashboard.url}")
+    private String kibanaUrl;
 
 	
 	@GetMapping("/") public String root() { return "login"; }
     @GetMapping("/login") public String login() { return "login"; }
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-    @GetMapping("/dashboard") public String dashboard() { return "auth/dashboard"; }
+    @GetMapping("/dashboard") public String dashboard( Model model, @ModelAttribute("attributes") Map<?,?> attributes) {
+        model.addAttribute("kibanaUrl", kibanaUrl);
+        return "auth/dashboard"; 
+    }
 
 
     @GetMapping("scanondemandindex") public String scanOnDemand() { return "auth/scanondemand/index"; }
